e35e62ae2aa4ec474fa044048ba794481eb7c065
Thura <thura.00011@gmail.com>
2024-04-10 22:52:35 +0630

    Add Main Navbar

---
+++ app/[lang]/components/MainNavbar.jsx
@@ -0,0 +1,82 @@
+"use client"
+
+import React from "react";
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+
+export default function MainNavbar() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const menuItems = [
+        "Home",
+        "About",
+        "Experience",
+        "Skills",
+        "Contact",
+    ];
+
+    return (
+        <Navbar
+            isBordered
+            isMenuOpen={isMenuOpen}
+            onMenuOpenChange={setIsMenuOpen}
+        >
+            <NavbarContent className="sm:hidden" justify="start">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+            </NavbarContent>
+
+            <NavbarContent className="sm:hidden pr-3" justify="center">
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">MY PORTFOLIO</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="end">
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">MY PORTFOLIO</p>
+                </NavbarBrand>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Home
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        About
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Experience
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Skills
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Contact
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            className="w-full"
+                            color={
+                                index === 2 ? "warning" : "foreground"
+                            }
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
+}
--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,14 +1,18 @@
 import LocaleSwitcher from "./components/locale-switcher";
 import { getDictionary } from "./dictionaries";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
+import MainNavbar from "./components/MainNavbar";
 
 export default async function Home({ params: { lang } }) {
   const dict = await getDictionary(lang)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>{dict.about.welcome}</p>
-      <ThemeSwitcher/>
-      <LocaleSwitcher />
-    </main>
+    <>
+      <MainNavbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>{dict.about.welcome}</p>
+        <ThemeSwitcher />
+        <LocaleSwitcher />
+      </main>
+    </>
   );
 }
