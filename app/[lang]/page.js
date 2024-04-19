ee8b6a16bd03df1471bc8fc20f8b9449198aa009
Thura <thura.00011@gmail.com>
2024-04-19 23:14:40 +0630

    Update contents for experience section

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -47,31 +47,52 @@ export default async function Home({ params: { lang } }) {
           <div className="container w-4/5 mx-auto py-5 sm:p-10">
             <h1 className="text-3xl font-bold">Experience</h1>
             <div className="p-3 sm:p-8 space-y-2">
-              <h3 className="text-lg font-bold">Atlantbh</h3>
-              <h1 className="text-xl font-bold">Junior Software Engineer</h1>
-              <p className="text-sm italic">Jun 2022 - Jan 2024</p>
-              <ul className="list-disc leading-7">
-                <li>Developed a full-stack enterprise applications using React, PostgreSQL and Docker.</li>
-                <li>Developed a full-stack enterprise applications using React, PostgreSQL and Docker.</li>
-                <li>Provided mentorship to two IT college students, enhancing their practical software engineering skills.</li>
+              <h3 className="text-lg font-semibold">AmbitBound Technology Co. Ltd</h3>
+              <h1 className="text-xl font-bold">Full-stack Developer</h1>
+              <p className="text-sm italic">July 2023 - Present</p>
+              <ul className="list-disc leading-7 text-justify">
+                <li>Lead a team of developers in the design, development, testing, and
+                  deployment of software solutions.</li>
+                <li>Reviewed code, architecture, and design decisions to ensure adherence
+                  to best practices and coding standards.</li>
+                <li>Developed responsive and user-friendly web applications using React.js
+                  for the front end and Node.js for the back end.</li>
+                <li>Implemented features and functionality according to technical specifications
+                  and project requirements.</li>
+                <li>Optimized web applications for maximum performance and scalability.</li>
               </ul>
             </div>
             <div className="p-3 sm:p-8 space-y-2">
-              <h1 className="text-xl font-bold">Atlantbh</h1>
-              <h1 className="text-xl font-bold">Junior Software Engineer</h1>
-              <ul className="list-disc leading-7">
-                <li>Developed a full-stack enterprise applications using React, PostgreSQL and Docker.</li>
-                <li>Developed a full-stack enterprise applications using React, PostgreSQL and Docker.</li>
-                <li>Provided mentorship to two IT college students, enhancing their practical software engineering skills.</li>
+              <h3 className="text-lg font-semibold">7th Computing Co. Ltd</h3>
+              <h1 className="text-xl font-bold">Developer</h1>
+              <p className="text-sm italic">Jan 2023 - July 2023</p>
+              <ul className="list-disc leading-7 text-justify">
+                <li>Analyzed client requirements and translate them into technical specifications
+                  for Odoo module development</li>
+                <li>Customized and extend existing Odoo modules to meet the unique needs
+                  of individual clients.</li>
+                <li>Developed new Odoo modules from scratch, adhering to best practices and
+                  coding standards.</li>
+                <li>Integrated Odoo with third-party systems and applications.</li>
+                <li>Perform unit testing and debugging to ensure the quality and
+                  stability of Odoo implementations.</li>
+                <li>Developed comprehensive enterprise reports utilizing intricate SQL queries
+                  within the Odoo framework.</li>
               </ul>
             </div>
             <div className="p-3 sm:p-8 space-y-2">
-              <h1 className="text-xl font-bold">Atlantbh</h1>
-              <h1 className="text-xl font-bold">Junior Software Engineer</h1>
-              <ul className="list-disc leading-7">
-                <li>Developed a full-stack enterprise applications using React, PostgreSQL and Docker.</li>
-                <li>Developed a full-stack enterprise applications using React, PostgreSQL and Docker.</li>
-                <li>Provided mentorship to two IT college students, enhancing their practical software engineering skills.</li>
+              <h3 className="text-lg font-semibold">Frontiir Co. Ltd</h3>
+              <h1 className="text-xl font-bold">Network Engineer</h1>
+              <p className="text-sm italic">Apr 2022 - Oct 2022</p>
+              <ul className="list-disc leading-7 text-justify">
+                <li>Assist in the deployment and configuration of network equipment,
+                  including routers, switches, firewalls, and access points.</li>
+                <li>Conduct routine monitoring and maintenance of network systems to
+                  identify and resolve performance issues or security vulnerabilities.</li>
+                <li>Assist in the planning and execution of network expansion projects,
+                  including site surveys, capacity planning, and network optimization.</li>
+                <li>Provided technical support and assistance to internal teams and
+                  external customers.</li>
               </ul>
             </div>
           </div>
