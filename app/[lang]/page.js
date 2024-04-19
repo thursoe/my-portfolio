59c53df19fb0b2c89c03f1f78f4c8d3c31067b07
Thura <thura.00011@gmail.com>
2024-04-19 22:42:49 +0630

    Update  margin and padding for responsive design

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -20,7 +20,7 @@ export default async function Home({ params: { lang } }) {
           <div className="grid sm:grid-cols-2 container w-4/5 h-[calc(100vh-64px)] mx-auto">
             <div className="grid place-content-center gap-2">
               <h2 className="text-lg font-light">Hello! This is Thura.</h2>
-              <h1 className="text-xl font-bold typing-animation after:content-['|'] after:animate-ping">A Passionate Software Engineer</h1>
+              <h1 className="text-lg sm:text-xl font-bold typing-animation after:content-['|'] after:animate-ping">A Passionate Software Engineer</h1>
               <div className="flex gap-2 mt-4">
                 <Button color="secondary" className="animate-pulse">Download CV</Button>
                 <Button>Contact Me</Button>
@@ -44,9 +44,9 @@ export default async function Home({ params: { lang } }) {
           </div>
         </section>
         <section id="experience">
-          <div className="container w-4/5 mx-auto p-10">
+          <div className="container w-4/5 mx-auto py-5 sm:p-10">
             <h1 className="text-3xl font-bold">Experience</h1>
-            <div className="p-8 space-y-2">
+            <div className="p-3 sm:p-8 space-y-2">
               <h3 className="text-lg font-bold">Atlantbh</h3>
               <h1 className="text-xl font-bold">Junior Software Engineer</h1>
               <p className="text-sm italic">Jun 2022 - Jan 2024</p>
@@ -56,7 +56,7 @@ export default async function Home({ params: { lang } }) {
                 <li>Provided mentorship to two IT college students, enhancing their practical software engineering skills.</li>
               </ul>
             </div>
-            <div className="p-8 space-y-2">
+            <div className="p-3 sm:p-8 space-y-2">
               <h1 className="text-xl font-bold">Atlantbh</h1>
               <h1 className="text-xl font-bold">Junior Software Engineer</h1>
               <ul className="list-disc leading-7">
@@ -65,7 +65,7 @@ export default async function Home({ params: { lang } }) {
                 <li>Provided mentorship to two IT college students, enhancing their practical software engineering skills.</li>
               </ul>
             </div>
-            <div className="p-8 space-y-2">
+            <div className="p-3 sm:p-8 space-y-2">
               <h1 className="text-xl font-bold">Atlantbh</h1>
               <h1 className="text-xl font-bold">Junior Software Engineer</h1>
               <ul className="list-disc leading-7">
@@ -77,13 +77,13 @@ export default async function Home({ params: { lang } }) {
           </div>
         </section>
         <section id="skills">
-          <div className="w-4/5 sm:h-screen mx-auto pb-14 grid place-content-center">
+          <div className="w-4/5 sm:h-screen mx-auto pb-14 pt-5 grid place-content-center">
             <h1 className="text-center text-3xl font-bold mb-10">Expertise and skills</h1>
             <CardGrid />
           </div>
         </section>
         <section id="contact">
-          <div className="w-4/5 sm:h-screen mx-auto flex items-center justify-center pb-14">
+          <div className="w-4/5 sm:h-screen mx-auto flex items-center justify-center pb-14 pt-5">
             <ContactSection />
           </div>
         </section>
